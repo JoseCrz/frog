584b165d6685ffaeb8f54c1e9a1178ec6b59dc3a
Jose Cuevas <josecueram@gmail.com>
2020-09-03 16:21:59 -0500

    [UPDATE] Moved Font Link to HTML Template

--- src/components/Layout/index.jsx
+++ src/components/Layout/index.jsx
@@ -8,7 +8,6 @@ export const Layout = ({ children, title }) => {
     <>
       <Helmet>
         <title>{`Frog | ${title}`}</title>
-        <link href='https://fonts.googleapis.com/css2?family=Comfortaa&family=Roboto:wght@400;700&display=swap' rel='stylesheet' />
       </Helmet>
       <Main>
         {
