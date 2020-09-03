1830eb7114caaf462f71d9d322bad6f724c2f1ab
Jose Cuevas <josecueram@gmail.com>
2020-09-03 16:15:28 -0500

    [ADD] Created basic Home Page

---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import { Layout } from '../../components/Layout'
+export const Home = () => {
+  return (
+    <Layout title='Home'>
+      <h1>Hello there</h1>
+    </Layout>
+  )
+}
