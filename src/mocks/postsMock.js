04f82a0d52ee6ff655505c43807bb7b60e0f7c6c
Jose Cuevas <josecueram@gmail.com>
2020-09-04 02:16:11 -0500

    [UPDATE] postMock username examples

--- src/mocks/postsMock.js
+++ src/mocks/postsMock.js
@@ -3,28 +3,28 @@ export const postsMock = [
     id: '1',
     pictureUrl: 'https://images.unsplash.com/photo-1599018552443-2d2274c76b7b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1275&q=80',
     userName: 'Ridhwan Nordin',
-    userHandle: '@ridzjcob',
+    userHandle: 'ridzjcob',
     userPicture: 'https://images.unsplash.com/profile-1599016869734-368f081f8e5eimage?dpr=2&auto=format&fit=crop&w=150&h=150&q=60&crop=faces&bg=fff'
   },
   {
     id: '2',
     pictureUrl: 'https://images.unsplash.com/photo-1599018552443-2d2274c76b7b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1275&q=80',
     userName: 'Ridhwan Nordin',
-    userHandle: '@ridzjcob',
+    userHandle: 'ridzjcob',
     userPicture: 'https://images.unsplash.com/profile-1599016869734-368f081f8e5eimage?dpr=2&auto=format&fit=crop&w=150&h=150&q=60&crop=faces&bg=fff'
   },
   {
     id: '3',
     pictureUrl: 'https://images.unsplash.com/photo-1599018552443-2d2274c76b7b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1275&q=80',
     userName: 'Ridhwan Nordin',
-    userHandle: '@ridzjcob',
+    userHandle: 'ridzjcob',
     userPicture: 'https://images.unsplash.com/profile-1599016869734-368f081f8e5eimage?dpr=2&auto=format&fit=crop&w=150&h=150&q=60&crop=faces&bg=fff'
   },
   {
     id: '4',
     pictureUrl: 'https://images.unsplash.com/photo-1599018552443-2d2274c76b7b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1275&q=80',
     userName: 'Ridhwan Nordin',
-    userHandle: '@ridzjcob',
+    userHandle: 'ridzjcob',
     userPicture: 'https://images.unsplash.com/profile-1599016869734-368f081f8e5eimage?dpr=2&auto=format&fit=crop&w=150&h=150&q=60&crop=faces&bg=fff'
   }
 ]
