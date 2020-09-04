b96a3824c84dbb285f7f535e167ecbd1e76b07a9
Jose Cuevas <josecueram@gmail.com>
2020-09-04 11:21:40 -0500

    [ADD] PropTypes to Header

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
+import PropTypes from 'prop-types'
+
 import { H1 } from './styles'
+
 export const Header = ({ children }) => {
   return (
     <header>
       <H1>{children}</H1>
     </header>
   )
+}
+
+Header.propTypes = {
+  children: PropTypes.node.isRequired
 }
