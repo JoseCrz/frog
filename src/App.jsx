718fad252c1448fe93f0ebb7c7ad44447c196356
Jose Cuevas <josecueram@gmail.com>
2020-09-03 20:56:28 -0500

    [ADD] useFixNavbarBug hook

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,12 @@ import { Router } from '@reach/router'
 
 import { Home } from './pages/Home'
 
+import { useFixNavbarBug } from './hooks/useFixNavbarBug'
+
 import { GlobalStyle } from './styles/GlobalStyle'
 
 export const App = () => {
+  useFixNavbarBug()
   return (
     <>
       <GlobalStyle />
