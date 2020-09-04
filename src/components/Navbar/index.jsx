4a0d1c15f8f9ac9e327e0055ef81aee8b77c43a8
Jose Cuevas <josecueram@gmail.com>
2020-09-03 21:21:45 -0500

    [ADD] Navbar Icons

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,10 +1,38 @@
 import React from 'react'
+import { Link } from '@reach/router'
+import { IconContext } from 'react-icons'
+import { AiOutlineHome, AiFillHome } from 'react-icons/ai'
+import { BsChatDots, BsChatDotsFill } from 'react-icons/bs'
+import { RiUserLine, RiUserFill, RiSearchLine, RiSearchFill } from 'react-icons/ri'
 
-import { Nav } from './styles'
+import { Nav, Ul, Li } from './styles'
 export const Navbar = () => {
   return (
-    <Nav>
-      Navbar
-    </Nav>
+    <IconContext.Provider value={{ color: 'var(--text-color)', size: '24px' }}>
+      <Nav>
+        <Ul>
+          <Li>
+            <Link to='/'>
+              <AiFillHome />
+            </Link>
+          </Li>
+          <Li>
+            <Link to='/'>
+              <RiSearchLine />
+            </Link>
+          </Li>
+          <Li>
+            <Link to='/'>
+              <BsChatDots />
+            </Link>
+          </Li>
+          <Li>
+            <Link to='/'>
+              <RiUserLine />
+            </Link>
+          </Li>
+        </Ul>
+      </Nav>
+    </IconContext.Provider>
   )
 }
--- src/components/Navbar/styles.js
+++ src/components/Navbar/styles.js
@@ -2,5 +2,19 @@ import styled from 'styled-components'
 
 export const Nav = styled.nav`
   grid-area: navbar;
-  background: gray;
+  background: var(--background);
+  border-top: 1px solid lightgray;
+`
+
+export const Ul = styled.ul`
+  height: 100%;
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  grid-template-rows: 1fr;
+`
+
+export const Li = styled.li`
+  display: flex;
+  justify-content: center;
+  align-items: center;
 `
