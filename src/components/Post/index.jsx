219fe38fd769cf118824451728aeba18dac76eed
Jose Cuevas <josecueram@gmail.com>
2020-09-04 03:45:56 -0500

    [ADD] activateModal functionality in Post Component

--- src/components/Post/index.jsx
+++ src/components/Post/index.jsx
@@ -1,4 +1,5 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { Context } from '../../Context'
 
 import { useNearScreen } from '../../hooks/useNearScreen'
 
@@ -6,14 +7,19 @@ import { UserDetails } from '../UserDetails'
 import { PostContainer, Img, ImgContainer } from './styles'
 
 export const Post = ({ post }) => {
+  const { activateModal } = useContext(Context)
+
   const [show, element] = useNearScreen()
 
+  const handleOnClick = () => activateModal({ picture: post })
+
   const { urls, user } = post
+
   return (
     <PostContainer ref={element}>
       <ImgContainer>
         {
-          show && <Img src={urls.regular} />
+          show && <Img onClick={handleOnClick} src={urls.regular} />
         }
       </ImgContainer>
       <UserDetails name={user.name} username={user.username} profileImage={user.profile_image.medium} />
