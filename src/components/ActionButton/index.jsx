32a15e441fd4a7905c4c43949eed0e8fd48bb896
Jose Cuevas <josecueram@gmail.com>
2020-09-04 00:22:30 -0500

    [ADD] Created ActionButton Component

---
+++ src/components/ActionButton/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { AiOutlinePlus as PlusIcon } from 'react-icons/ai'
+import { Button } from './styles'
+export const ActionButton = () => {
+  return (
+    <Button>
+      <PlusIcon color='white' />
+    </Button>
+  )
+}
---
+++ src/components/ActionButton/styles.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components'
+
+export const Button = styled.div`
+  background: var(--accent-gradient);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 70%;
+  border-radius: 20px;
+`
