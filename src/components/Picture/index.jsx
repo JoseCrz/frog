a10cc9b4f80762250d4e5da88469c2b8ed7a435a
Jose Cuevas <josecueram@gmail.com>
2020-09-04 00:05:40 -0500

    [ADD] Implemented Lazy Load in Picture Component

--- src/components/Picture/index.jsx
+++ src/components/Picture/index.jsx
@@ -1,12 +1,17 @@
 import React from 'react'
+import { useNearScreen } from '../../hooks/useNearScreen'
 
 import { ImgContainer, Img } from './styles'
+
 export const Picture = ({ urls, width, height }) => {
+  const [show, element] = useNearScreen()
   const aspectRatio = (height / width) * 100
 
   return (
-    <ImgContainer aspectRatio={aspectRatio}>
-      <Img src={urls.small} />
+    <ImgContainer ref={element} aspectRatio={aspectRatio}>
+      {
+        show && <Img src={urls.small} />
+      }
     </ImgContainer>
   )
 }
