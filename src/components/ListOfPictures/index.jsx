eb5ea3a8734c9b2d2575203e352369331cda5fd8
Jose Cuevas <josecueram@gmail.com>
2020-09-04 05:24:03 -0500

    [ADD] Implemented Masonry

--- src/components/ListOfPictures/index.jsx
+++ src/components/ListOfPictures/index.jsx
@@ -1,16 +1,18 @@
 import React from 'react'
+import Masonry from 'react-responsive-masonry'
 import { Picture } from '../Picture'
 
-import { Grid, P } from './styles'
+import { P } from './styles'
 export const ListOfPictures = ({ pictures, listTitle }) => {
+  console.log('ListOfPictures -> pictures', pictures)
   return (
     <>
       <P>{listTitle}</P>
-      <Grid>
+      <Masonry columnsCount={2} gutter='10px'>
         {
           pictures.map(picture => <Picture key={picture.id} picture={picture} />)
         }
-      </Grid>
+      </Masonry>
     </>
   )
 }
--- src/components/Picture/index.jsx
+++ src/components/Picture/index.jsx
@@ -4,20 +4,23 @@ import { useNearScreen } from '../../hooks/useNearScreen'
 
 import { ImgContainer, Img } from './styles'
 
-export const Picture = ({ picture }) => {
-  const { activateModal } = useContext(Context)
-  const [show, element] = useNearScreen()
+export const Picture = React.memo(
+  ({ picture }) => {
+    const { activateModal } = useContext(Context)
+    const [show, element] = useNearScreen()
 
-  const { height, width, urls } = picture
-  const aspectRatio = (height / width) * 100
+    const { height, width, urls } = picture
+    const aspectRatio = (height / width) * 100
 
-  const handleOnClick = () => activateModal({ picture })
+    const handleOnClick = () => activateModal({ picture })
+    console.log('Picture -> picture', picture)
 
-  return (
-    <ImgContainer ref={element} aspectRatio={aspectRatio}>
-      {
-        show && <Img onClick={handleOnClick} src={urls.regular} />
-      }
-    </ImgContainer>
-  )
-}
+    return (
+      <ImgContainer ref={element} aspectRatio={aspectRatio}>
+        {
+          show && <Img onClick={handleOnClick} src={urls.regular} />
+        }
+      </ImgContainer>
+    )
+  }
+)
