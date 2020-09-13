fb5a40db2974c019d574d455bb0bebe431d1ad12
Jose Cuevas <josecueram@gmail.com>
2020-09-13 02:37:11 -0500

    [FIX] Modal Not Closing Bug

--- src/components/PictureDetails/index.jsx
+++ src/components/PictureDetails/index.jsx
@@ -1,12 +1,18 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { Link } from '@reach/router'
 import PropTypes from 'prop-types'
+
+import { Context } from '../../Context'
 
 import { Details, ProfileImg, Name, Username } from './styles'
 
 export const PictureDetails = ({ profileImage, name, username, darkBackground }) => {
+  const { deactivateModal } = useContext(Context)
+
+  const handleOnClick = () => deactivateModal()
+
   return (
-    <Link to={`/user/${username}`}>
+    <Link to={`/user/${username}`} onClick={(handleOnClick)}>
       <Details darkBackground={darkBackground}>
         <ProfileImg src={profileImage} alt='' />
         <div>
