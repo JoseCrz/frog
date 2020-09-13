0822d41e12a7f23c00ba9f003038c2654fb150ad
Jose Cuevas <josecueram@gmail.com>
2020-09-13 01:14:55 -0500

    [ADD] Link to user Page in Picture Details

--- src/components/PictureDetails/index.jsx
+++ src/components/PictureDetails/index.jsx
@@ -1,17 +1,20 @@
 import React from 'react'
+import { Link } from '@reach/router'
 import PropTypes from 'prop-types'
 
 import { Details, ProfileImg, Name, Username } from './styles'
 
 export const PictureDetails = ({ profileImage, name, username, darkBackground }) => {
   return (
-    <Details darkBackground={darkBackground}>
-      <ProfileImg src={profileImage} alt='' />
-      <div>
-        <Name>{name}</Name>
-        <Username>@{username}</Username>
-      </div>
-    </Details>
+    <Link to={`/user/${username}`}>
+      <Details darkBackground={darkBackground}>
+        <ProfileImg src={profileImage} alt='' />
+        <div>
+          <Name>{name}</Name>
+          <Username>@{username}</Username>
+        </div>
+      </Details>
+    </Link>
   )
 }
 
